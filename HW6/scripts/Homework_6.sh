f698158e457875ee2ea931ebbff62374cd93d2e6
Anujagagangras <agagangras9587@sdsu.edu>
2022-12-01 17:14:30 -0800

    error removed from the name od container specified in the bash file

--- HW6/scripts/Homework_6.sh
+++ HW6/scripts/Homework_6.sh
@@ -1,8 +1,9 @@
+#!/bin/bash
 
-sleep 15
+sleep 10
 
 if ! mysql -h mariadb -u root -e 'use baseball'; then
-    echo "Baseball DOES NOT exists"
+    echo "Baseball does not exists"
     mysql -h mariadb -u root -e "CREATE DATABASE IF NOT EXISTS baseball"
     echo "Importing database tables"
     mysql -h mariadb -u root baseball < /scripts/baseball.sql
@@ -13,5 +14,5 @@ fi
 echo "Calling Homework_6.sql"
 mysql -h mariadb -u root  baseball < /scripts/Homework_6.sql
 
-mysql -h my-db -u root baseball -e '
+mysql -h mariadb -u root baseball -e '
   SELECT * FROM Rolling_Average where game_id=12560;' > /results/output.txt
