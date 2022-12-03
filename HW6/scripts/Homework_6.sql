911f9f244d92e09cc2742d06732740c3dc5e1650
Anujagagangras <agagangras9587@sdsu.edu>
2022-12-02 19:45:42 -0800

    sql code changed to get rolling avg

--- HW6/scripts/Homework_6.sql
+++ HW6/scripts/Homework_6.sql
@@ -4,25 +4,28 @@ SHOW WARNINGS;
 USE baseball;
 SHOW TABLES;
 
-#calculation of rolling average
-DROP TABLE IF EXISTS Input_Table;  ## input table to calculate rolling average
-CREATE TABLE Input_Table as
-    select b.game_id,
-           b.batter,
-           g.local_date,
-           b.Hit,
-           b.atBat
-FROM game g JOIN batter_counts b on g.game_id = b.game_id
-having atBat>0;
+#create input table
+DROP TABLE IF EXISTS input_table;
+CREATE TABLE input_table as
+	 SELECT hit,
+	        atbat,
+	        batter,
+	        bc.game_id,
+	        Date(local_date) as l_date
+FROM batter_counts bc
+JOIN game g ON g.game_id = bc.game_id
+ORDER BY game_id ,l_date;
 
+# rolling average calculation for game id = 12560
 DROP TABLE IF EXISTS Rolling_Average;
 CREATE TABLE Rolling_Average as
-SELECT *,
-       AVG(Hit/atBat) OVER(
-           PARTITION BY batter
-           ORDER BY local_date
-           ROWS BETWEEN 99 PRECEDING AND CURRENT ROW
-           )
-        AS 100_days_rolling_average
-FROM Input_Table;
+	SELECT sum(ifnull(it1.Hit,0))/nullif(sum(ifnull(it1.atbat,0)),0) as Rolling_average,
+	       it.l_date,
+	       it.game_id,
+	       it.batter,
+	       count(*) as cnt
+# self joining input table
+FROM input_table it JOIN input_table it1 ON it1.batter = it.batter AND it1.l_date BETWEEN DATE_ADD(it.l_date, interval -100 day)
+AND DATE_SUB(it.l_date, INTERVAL 1 DAY) WHERE it.game_id = 12560
+GROUP BY it.game_id ,it.batter, it.l_date;
 
